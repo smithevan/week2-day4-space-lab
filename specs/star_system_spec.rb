b4d491f158f600c8ffdbcf6330d198d25af7e9fb
Evan Smith <smith.m.evan@gmail.com>
2019-11-28 15:04:33 +0000

    finished pmv distance of planets

--- specs/star_system_spec.rb
+++ specs/star_system_spec.rb
@@ -45,27 +45,28 @@ class TestStarSystem < Minitest::Test
     assert_equal(@jupiter, result)
   end
   #
-  # def test_get_smallest_planet
-  #   result = @solar_system.get_smallest_planet
-  #   assert_equal(@pluto, result)
-  # end
+  def test_get_smallest_planet
+    result = @solar_system.get_smallest_planet
+    assert_equal(@pluto, result)
+  end
   #
-  # def test_get_planets_with_no_moons
-  #   expected_planets = [ @mercury, @venus ]
-  #   result = @solar_system.get_planets_with_no_moons
-  #   assert_equal(expected_planets, result)
-  # end
+  def test_get_planets_with_no_moons
+    expected_planets = [ @mercury, @venus ]
+    result = @solar_system.get_planets_with_no_moons
+    assert_equal(expected_planets, result)
+  end
   #
-  # def test_get_names_of_planet_with_more_than_four_moons
-  #   expected_names = ["Jupiter", "Neptune", "Saturn", "Uranus"]
-  #   result = @solar_system.get_planets_with_more_moons(4)
-  #   assert_equal(expected_names, result)
-  # end
+  def test_get_names_of_planet_with_more_than_four_moons
+    expected_names = ["Jupiter", "Neptune", "Saturn", "Uranus"]
+    result = @solar_system.get_planets_with_more_moons(4)
+
+    assert_equal(expected_names, result)
+  end
   #
-  # def test_number_of_planets_less_than_1billion_km_from_sun
-  #   result = @solar_system.get_number_of_planets_closer_than(1000)
-  #   assert_equal(5, result)
-  # end
+  def test_number_of_planets_less_than_1billion_km_from_sun
+    result = @solar_system.get_number_of_planets_closer_than(1000)
+    assert_equal(5, result)
+  end
 
   # OPTIONAL - MAY REQUIRE LOOKING UP THE DOCS
 
--- star_system.rb
+++ star_system.rb
@@ -6,8 +6,8 @@ class StarSystem
    @planets = planets
  end
 
- def planet_names
-   @planets.map { |planet| planet.name}
+ def planet_names(planets = @planets)
+   planets.map { |planet| planet.name}
  end
 
  def get_planet_by_name(name_we_want)
@@ -18,6 +18,27 @@ class StarSystem
    planet_diameters = @planets.map { |planet| planet.diameter }
    target_planet_diameter = planet_diameters.max
    @planets.find {|planet| planet.diameter == target_planet_diameter}
+ end
+
+ def get_smallest_planet
+   planet_diameters = @planets.map { |planet| planet.diameter }
+   target_planet_diameter = planet_diameters.min
+   @planets.find {|planet| planet.diameter == target_planet_diameter}
+ end
+
+ def get_planets_with_no_moons
+   #planet_moon_data = @planets.map { |planet| planet.number_of_moons }
+   @planets.find_all {|planet| planet.number_of_moons == 0}
+ end
+
+ def get_planets_with_more_moons(desired_number_of_moons)
+   whole_planets_with_moons = @planets.find_all {|planet| planet.number_of_moons > desired_number_of_moons}
+   planet_names(whole_planets_with_moons)
+ end
+
+ def get_number_of_planets_closer_than(distance)
+   planets_close_to_sun = @planets.find_all {|planet| planet.distance_from_sun < distance}
+   return planets_close_to_sun.length
  end
 
 end
